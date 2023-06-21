2572990b83b0ddc50141e2c0c5385650a76def24
Daniel McKnight <daniel@neongecko.com>
2023-06-21 08:59:51 -0700

    Cleanup outdated docstring

--- ovos_PHAL_plugin_oauth/__init__.py
+++ ovos_PHAL_plugin_oauth/__init__.py
@@ -102,35 +102,8 @@ class OAuthPlugin(PHALPlugin):
         # trigger register events from oauth skills
         self.bus.emit(Message("oauth.ping"))
 
-
     @classproperty
     def runtime_requirements(self):
-        """ developers should override this if they do not require connectivity
-         some examples:
-         IOT plugin that controls devices via LAN could return:
-            scans_on_init = True
-            NetworkRequirements(internet_before_load=False,
-                                 network_before_load=scans_on_init,
-                                 requires_internet=False,
-                                 requires_network=True,
-                                 no_internet_fallback=True,
-                                 no_network_fallback=False)
-         online search plugin with a local cache:
-            has_cache = False
-            NetworkRequirements(internet_before_load=not has_cache,
-                                 network_before_load=not has_cache,
-                                 requires_internet=True,
-                                 requires_network=True,
-                                 no_internet_fallback=True,
-                                 no_network_fallback=True)
-         a fully offline plugin:
-            NetworkRequirements(internet_before_load=False,
-                                 network_before_load=False,
-                                 requires_internet=False,
-                                 requires_network=False,
-                                 no_internet_fallback=True,
-                                 no_network_fallback=True)
-        """
         return RuntimeRequirements(internet_before_load=True,
                                    network_before_load=True,
                                    gui_before_load=False,
